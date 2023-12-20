b08a10259b38902e2c310a8a4dd8230bbc71c723
Chongsawad Saiaram <chongsawad.saiaram@thoughtworks.com>
2023-12-20 13:31:35 +0700

    Initial Commit

---
+++ BrainDays/BrainDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainDaysApp.swift
+//  BrainDays
+//
+//  Created by Chongsawad Saiaram on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BrainDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
